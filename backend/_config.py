ad6dff243ee703c3e837c648f14afd8183f2cb8d
shanmukh1905 <shanmukhasai996@gmail.com>
2025-05-14 11:37:25 +0530

    cofigure the app

--- backend/_config.py
+++ backend/_config.py
@@ -5,7 +5,7 @@ DB_HOST = "127.0.0.1"
 DB_PORT = 3306
 DB_NAME = "stocks_view"
 DB_USER = "root"
-DB_PASSWORD = ""
+DB_PASSWORD = "Sai@96033"
 
 # Note: If deploying to production, consider using environment variables
 # instead of hardcoding credentials here
